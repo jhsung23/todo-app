ff4a93d18382d6f67d7aa0888da7568ead1c0282
sungjihyun <jhsung23@gmail.com>
2023-05-09 23:00:16 +0900

    feat: TaskItem 컴포넌트

---
+++ client/src/components/TaskItem.js
@@ -0,0 +1,91 @@
+import { FaRegStar, FaStar } from 'react-icons/fa';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+
+import { mapItemToComponent } from '../lib/mapItemToComponent';
+import { toggle, star } from '../module/task';
+
+const Container = styled.div`
+  width: 98%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  margin: 10px 0;
+`;
+
+const CheckboxWrapper = styled.div`
+  flex-basis: 1;
+
+  & input {
+    width: 1.1rem;
+    height: 1.1rem;
+  }
+`;
+
+const ContentWrapper = styled.div`
+  flex-grow: 1;
+  padding-left: 15px;
+  display: flex;
+  flex-direction: column;
+
+  & p {
+    margin: 0;
+    padding: 2px;
+  }
+
+  & .content {
+    font-size: 1rem;
+    font-weight: 500;
+  }
+
+  & .tag {
+    font-size: 0.8rem;
+    color: #909090;
+  }
+
+  &.done .content {
+    color: #909090;
+    text-decoration: line-through;
+  }
+`;
+
+const StarWrapper = styled.div`
+  flex-basis: 1;
+
+  & .not-starred {
+    color: #bbbbbb;
+  }
+
+  & .starred {
+    color: #1a85ff;
+  }
+`;
+
+const TaskItem = ({ task, setClickedItem }) => {
+  const { id, name, isDone, isImportant, tag } = task;
+  const dispatch = useDispatch();
+
+  const handleToggle = (e) => {
+    dispatch(toggle(id));
+  };
+  const handleStar = (e) => {
+    dispatch(star(id));
+  };
+
+  return (
+    <Container>
+      <CheckboxWrapper>
+        <input type="checkbox" checked={isDone} onChange={handleToggle} id={id} />
+      </CheckboxWrapper>
+      <ContentWrapper className={isDone ? 'done' : undefined} onClick={(e) => setClickedItem(id)}>
+        <p className="content">{name}</p>
+        <p className="tag">{mapItemToComponent[tag] + ' ' + tag}</p>
+      </ContentWrapper>
+      <StarWrapper onClick={handleStar}>
+        {isImportant ? <FaStar className="starred" /> : <FaRegStar className="not-starred" />}
+      </StarWrapper>
+    </Container>
+  );
+};
+
+export default TaskItem;
