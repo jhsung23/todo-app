fa3aaf51588746f18ff515b942e5343373169e92
sungjihyun <jhsung23@gmail.com>
2023-05-10 20:21:28 +0900

    feat: 라우팅될 페이지 컴포넌트

---
+++ client/src/pages/TaskPage.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+import Header from '../components/Header';
+import TaskItem from '../components/TaskItem';
+import { useSelector } from 'react-redux';
+
+const Container = styled.div`
+  padding: 20px;
+  padding-top: 40px;
+  height: 100vh;
+`;
+
+const TaskPage = ({ category, tag }) => {
+  const { tasks } = useSelector((state) => state.task);
+  let taskData;
+  let pageTitle = category || tag;
+
+  if (category === 'tasks') {
+    taskData = tasks;
+  } else if (category === 'important') {
+    taskData = tasks.filter((task) => task.isImportant);
+  } else if (category === 'completed') {
+    taskData = tasks.filter((task) => task.isDone);
+  } else {
+    taskData = tasks.filter((task) => task.tag === tag);
+  }
+
+  return (
+    <Container>
+      <Header title={pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1)} />
+      {taskData.map((task) => (
+        <TaskItem key={task.id} task={task} />
+      ))}
+    </Container>
+  );
+};
+
+export default TaskPage;
