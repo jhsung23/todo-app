8274f687ac99c72965c13e8ea7edaeaed54bf579
sungjihyun <jhsung23@gmail.com>
2023-05-11 23:17:25 +0900

    feat: 태스크가 없는 경우 대체 이미지

--- client/src/pages/TaskPage.js
+++ client/src/pages/TaskPage.js
@@ -5,11 +5,30 @@ import { useDispatch, useSelector } from 'react-redux';
 import Header from '../components/Header';
 import TaskItem from '../components/TaskItem';
 import { open, change } from '../module/modal';
+import { ReactComponent as Nothing } from '../img/Nothing.svg';
 
 const Container = styled.div`
   padding: 20px;
   padding-top: 40px;
   height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & .nothing {
+    width: 200px;
+    height: 200px;
+    margin-top: 100px;
+  }
+
+  & p {
+    color: #777;
+    font-size: 1.1rem;
+  }
 `;
 
 const TaskPage = () => {
@@ -40,9 +59,14 @@ const TaskPage = () => {
   return (
     <Container onClick={closeModal}>
       <Header title={pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1)} />
-      {taskData.map((task) => (
-        <TaskItem key={task.id} task={task} />
-      ))}
+      {taskData.length ? (
+        taskData.map((task) => <TaskItem key={task.id} task={task} />)
+      ) : (
+        <Wrapper>
+          <Nothing className="nothing" />
+          <p>There's nothing to do.</p>
+        </Wrapper>
+      )}
     </Container>
   );
 };
